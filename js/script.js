1d1e68baa65bb6571ea2f864347b6505fbc8f25d
Izy Dempsey <117126818+izydempsey@users.noreply.github.com>
2023-05-14 21:54:36 -0400

    Added a limit to the guest list, added and removed some classes to show limit reached

--- js/script.js
+++ js/script.js
@@ -34,4 +34,11 @@ const addToList = function (guest) {
 const updateGuestCount = function () {
     const guests = document.querySelectorAll(".guest-list li");
     guestCount.innerText = guests.length;
+
+    if (guests.length === 8) {
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
 }
